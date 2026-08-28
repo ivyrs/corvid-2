c43ea958807bd7cde5ee0f954ec5654ea2beb4e3
ivy forever <ivy@ivy.rs>
2026-08-28 09:05:16 +0100

    tooling: move to devenv
    on [xela](https://xela.zone) and [tris](https://namtao.com)'s
    recommendation

---
+++ devenv.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [
+    pkgs.nodejs_22
+    pkgs.pnpm
+  ];
+
+  enterShell = ''
+    export PATH="$PWD/node_modules/.bin:$PATH"
+  '';
+
+  scripts = {
+    dev.exec = "pnpm run dev";
+    build.exec = "pnpm run build";
+    preview.exec = "pnpm run preview";
+    astro.exec = "pnpm run astro";
+    fmt.exec = "pnpm run fmt";
+    fmt-check.exec = "pnpm run fmt:check";
+    lint.exec = "pnpm run lint";
+  };
+}
